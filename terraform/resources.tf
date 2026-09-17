e4f8a2cf889197e2536b1c63039d62d0f6d3b515
Revathi <matta.revathi@gmail.com>
2026-09-17 08:32:06 +0530

    corrected the file name

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "ec2_role" {
 
 resource "aws_iam_role_policy_attachment" "ec2_role_policy_attachment" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSelasticbeanstalkWebTier"
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
